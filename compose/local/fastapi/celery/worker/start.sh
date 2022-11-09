1a9f7a1fae3c81793327119a7c1a60038646c8a0
Jason Jiang <Jason.Jiang@qbe.com>
2022-11-09 16:19:42 +1100

    multiple-queues and task routing

--- compose/local/fastapi/celery/worker/start.sh
+++ compose/local/fastapi/celery/worker/start.sh
@@ -3,6 +3,8 @@
 set -o errexit
 set -o nounset
 
+# celery -A main.celery worker --loglevel=info
+
 watchfiles \
   --filter python \
-  'celery -A main.celery worker --loglevel=info'
+  'celery -A main.celery worker --loglevel=info -Q high_priority,default'        # update
--- project/config.py
+++ project/config.py
@@ -2,12 +2,21 @@ import os
 import pathlib
 from functools import lru_cache
 
+from kombu import Queue
+
+
+def route_task(name, args, kwargs, options, task=None, **kw):
+    if ":" in name:
+        queue, _ = name.split(":")
+        return {"queue": queue}
+    return {"queue": "default"}
+
 
 class BaseConfig:
     BASE_DIR: pathlib.Path = pathlib.Path(__file__).parent.parent
 
     DATABASE_URL: str = os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR}/db.sqlite3")
-    DATABASE_CONNECT_DICT: dict = {"check_same_thread": False}  # only needed for sqlite
+    DATABASE_CONNECT_DICT: dict = {}
 
     CELERY_BROKER_URL: str = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
     CELERY_RESULT_BACKEND: str = os.environ.get("CELERY_RESULT_BACKEND", "redis://127.0.0.1:6379/0")
@@ -20,6 +29,21 @@ class BaseConfig:
         #     "schedule": 5.0,  # five seconds
         # },
     }
+
+    CELERY_TASK_DEFAULT_QUEUE: str = "default"
+
+    # Force all queues to be explicitly listed in `CELERY_TASK_QUEUES` to help prevent typos
+    CELERY_TASK_CREATE_MISSING_QUEUES: bool = False
+
+    CELERY_TASK_QUEUES: list = (
+        # need to define default queue here or exception would be raised
+        Queue("default"),
+
+        Queue("high_priority"),
+        Queue("low_priority"),
+    )
+
+    CELERY_TASK_ROUTES = (route_task,)
 
 
 class DevelopmentConfig(BaseConfig):
@@ -39,7 +63,7 @@ def get_settings():
     config_cls_dict = {
         "development": DevelopmentConfig,
         "production": ProductionConfig,
-        "testing": TestingConfig,
+        "testing": TestingConfig
     }
 
     config_name = os.environ.get("FASTAPI_CONFIG", "development")
